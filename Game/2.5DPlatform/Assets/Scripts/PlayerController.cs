0cb8a699e773e154cb42ef1be4376ce627d5ac0d
Meirith <meirith@gmail.com>
2018-12-14 00:18:34 +0100

    Game (Alpha) commit.

--- Game/2.5DPlatform/Assets/Scripts/PlayerController.cs
+++ Game/2.5DPlatform/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpForce;
     private float moveInput;
+    public bool facingRight = true;
 
     public bool isGrounded;
     public Transform feetPose;
@@ -20,8 +21,30 @@ public class PlayerController : MonoBehaviour
     public float jumpTime;
     private bool isJumping;
 
+    public float plumaSpeed = 600.0f;
+    public Transform plumaSpawn;
+    public Rigidbody plumaPrefab;
+
+    Rigidbody clone;
+
+    /*public Vector3 moveDirection;
+    public const float maxDashTime = 1.0f;
+    public float dashDistance = 10;
+    public float dashStoppingSpeed = 0.1f;
+    float currentDashTime = maxDashTime;
+    float dashSpeed = 6;
+
+    CharacterController controller;*/
 
     // Use this for initialization
+
+    void Awake()
+    {
+        /*controller = GetComponent<CharacterController>();*/
+        plumaSpawn = GameObject.Find("PlumaSpawn").transform;
+    }
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -36,24 +59,27 @@ public class PlayerController : MonoBehaviour
 
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector3(moveInput * speed, rb.velocity.y);
+        if (moveInput > 0.0f && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveInput < 0.0f && facingRight)
+        {
+            Flip();
+        }
+
+
+
+     
+
+        if (Input.GetKeyDown("c"))
+        {
+            Attack();
+        }
     }
 
     void Update()
     {
-
-
-
-
-        /*if(moveInput > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-
-        }
-        else if (moveInput < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }*/
-
 
 
 
@@ -81,7 +107,38 @@ public class PlayerController : MonoBehaviour
             isJumping = false;
 
         }
+        /*if (Input.GetKeyDown("x")){
 
+            currentDashTime = 0;
+
+        }
+        if (currentDashTime < maxDashTime)
+        {
+            currentDashTime += dashStoppingSpeed;
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+        controller.Move(moveDirection * Time.deltaTime * dashSpeed);*/
+    }
+
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up, 180.0f, Space.World);
+    }
+
+
+
+
+
+    void Attack()
+    {
+        clone = Instantiate(plumaPrefab, plumaSpawn.position, plumaSpawn.rotation) as Rigidbody;
+        clone.AddForce(plumaSpawn.transform.right * plumaSpeed);
     }
 
 }
---
+++ Game/2.5DPlatform/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField] int startingHealth = 90;
+    [SerializeField] float timeSinceLastHit = 2.0f;
+    [SerializeField] int currentHealth;
+
+    private PlayerController playerController;
+    private float timer = 0f;
+    private Animator anim;
+
+	public int CurrentHealth{
+		get { return currentHealth; }
+		set {
+			if (value < 0)
+				currentHealth = 0;
+			else
+				currentHealth = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+    {
+        //anim = GetComponent<Animator> ();
+        currentHealth = startingHealth;
+        playerController = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (timer >= timeSinceLastHit && !GameManager.instance.GameOver)
+        {
+            if (other.tag == "weapon")
+            {
+                takeHit();
+                timer = 0;
+            }
+        }
+    }
+
+    void takeHit()
+    {
+        if (currentHealth >= 0)
+        {
+            GameManager.instance.PlayerHit(currentHealth);
+            //anim.Play ("Hurt"); --> esto activa la animacion de recibir daño
+            currentHealth -= 30;
+        }
+        if (currentHealth <= 0)
+        {
+            GameManager.instance.PlayerHit(currentHealth);
+            //anim.SetTrigger ("isDead"); --> esto activa la animacion de muerte
+            playerController.enabled = false;
+        }
+    }
+
+
+	public void PowerUpHealth () {
+		if (currentHealth <= 60) {
+			currentHealth += 30;
+		} else if (currentHealth < startingHealth){
+			CurrentHealth = startingHealth;
+		}
+
+
+
+	}
+
+}
